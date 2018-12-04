af4ce690a74a44319a08e2ff61f055dd6fcd9f77
quyanchun <qyc199237@163.com>
2018-12-04 18:05:58 +0800

    test

---
+++ spring-cloud-eureka/src/main/java/com/yanchun/EurekaServer.java
@@ -0,0 +1,12 @@
+package com.yanchun;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author quyanchun
+ * @Date 2018/12/4
+ */
+@SpringBootApplication
+
+public class EurekaServer {
+}
