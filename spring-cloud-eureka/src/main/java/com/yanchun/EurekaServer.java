69d651735c53cc47a17e097c889652ada76008fd
quyanchun <qyc199237@163.com>
2018-12-05 16:14:08 +0800

    搭建环境

--- spring-cloud-eureka/src/main/java/com/yanchun/EurekaServer.java
+++ spring-cloud-eureka/src/main/java/com/yanchun/EurekaServer.java
@@ -1,12 +1,17 @@
 package com.yanchun;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author quyanchun
  * @Date 2018/12/4
  */
 @SpringBootApplication
-
+@EnableEurekaServer
 public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
 }
